6c65a570d6072e2d00831bb35641cfc1b6c4a4ea
Caleb Penning <calebpenning@mail.com>
2022-01-30 21:24:28 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int answer = rnd.Next(50);
+
+            Console.WriteLine(answer.ToString());
+
+
+
+            int guess = new int();
+
+            Console.WriteLine("Guess a number between 0 and 50!");
+
+            while (guess != answer)
+            {
+                guess = Int32.Parse(Console.ReadLine());
+                if (guess == answer)
+                {
+                    Console.WriteLine($"That's right! The answer was {answer}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{guess} isn't right. Try again!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
